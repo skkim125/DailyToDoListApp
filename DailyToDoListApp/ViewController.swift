05923b0b8c71727b81fcd1208de1e9163a811e55
skkim125 <kthanks125@gmail.com>
2024-07-02 16:12:44 +0900

    Initial Commit

---
+++ DailyToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyToDoListApp
+//
+//  Created by 김상규 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
